e5d4bb64889364087e693c810ebdacba3f86ec45
Vittorio Cozzolino <vittorio.cozzolino@huawei.com>
2023-10-30 16:00:34 +0100

    Add support for reading injected env variables.

--- hands-on/workflow-1/subscriber.py
+++ hands-on/workflow-1/subscriber.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 
+import os
 import platform
 import random
 from paho.mqtt import client as mqtt_client
 
 
-#broker = 'localhost'           # Broker in container should be used for the tutorial
-broker = 'broker.emqx.io'       # Free public MQTT broker
-port = 1883
-topic = 'test'
+# broker = 'localhost'           # Broker in container should be used for the tutorial
+# broker = 'broker.emqx.io'       # Free public MQTT broker
+# port = 1883
+# topic = 'test'
+broker = os.getenv("BROKER_ADDRESS", "broker.emqx.io")  # Free public MQTT broker
+port = int(os.getenv("BROKER_PORT", "1883"))
+topic = os.getenv("TOPIC", "test")
 node = platform.node()
 
 # Generate a Client ID with the subscribe prefix.
-client_id = f'subscriber-{random.randint(0, 100)}'
+client_id = f"subscriber-{random.randint(0, 100)}"
 
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print('Connected to MQTT Broker!')
+            print("Connected to MQTT Broker!")
         else:
-            print('Failed to connect, return code %d\n', rc)
+            print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
     client.on_connect = on_connect
@@ -30,7 +34,7 @@ def connect_mqtt() -> mqtt_client:
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
-        print(f'Received `{msg.payload.decode()}` from topc `{msg.topic}`')
+        print(f"Received `{msg.payload.decode()}` from topc `{msg.topic}`")
 
     client.subscribe(topic)
     client.on_message = on_message
@@ -42,5 +46,5 @@ def run():
     client.loop_forever()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
